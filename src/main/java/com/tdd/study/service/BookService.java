c1dca27f17d53eaa38a914a5edb8b61b3afea983
bmm522 <rlawldls11@naver.com>
2023-02-04 21:48:39 +0900

    feat : 북 서비스 책 등록 완료

--- src/main/java/com/tdd/study/service/BookService.java
+++ src/main/java/com/tdd/study/service/BookService.java
@@ -1,7 +1,23 @@
 package com.tdd.study.service;
 
+import com.tdd.study.domain.Book;
+import com.tdd.study.domain.BookRepository;
+import com.tdd.study.web.dto.BookResDto;
+import com.tdd.study.web.dto.BookSaveReqDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookResDto insertBook(BookSaveReqDto dto){
+        Book bookPs = bookRepository.save(dto.toEntity());
+        return new BookResDto().toDto(bookPs);
+    }
 }
--- src/main/java/com/tdd/study/web/dto/BookResDto.java
+++ src/main/java/com/tdd/study/web/dto/BookResDto.java
@@ -1,4 +1,21 @@
 package com.tdd.study.web.dto;
 
+import com.tdd.study.domain.Book;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BookResDto {
+
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResDto toDto(Book bookPS){
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
 }
