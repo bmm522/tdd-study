6132624256a92c54a8f5198bf90f0bb4899735e2
bmm522 <rlawldls11@naver.com>
2023-02-04 20:10:54 +0900

    feat : 책 등록 테스트 완료

--- src/test/java/com/tdd/study/domain/BookRepositoryTest.java
+++ src/test/java/com/tdd/study/domain/BookRepositoryTest.java
@@ -1,12 +1,17 @@
 package com.tdd.study.domain;
 
 import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @DataJpaTest // DB 와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
+
+
 
     @Autowired
     private BookRepository bookRepository;
@@ -14,6 +19,22 @@ public class BookRepositoryTest {
     @Test
     @DisplayName("책 등록 테스트")
     public void insertBookTest(){
-        System.out.println("책 등록 테스트 실행");
+        // given (데이터 준비)
+        String title = "junit5";
+        String author = "김지인";
+
+        Book book = Book.builder()
+                .title(title)
+                        .author(author)
+                                .build();
+
+        // when (테스트 실행)
+        Book bookPS = bookRepository.save(book);
+
+        // then (검증)
+
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+
     }
 }
