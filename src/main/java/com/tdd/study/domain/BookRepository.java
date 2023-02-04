546606cf9279aec5b6f06e2c0b44b3a5dd76d88d
bmm522 <rlawldls11@naver.com>
2023-02-04 17:13:08 +0900

    feat : 책 등록 테스트 실행

--- src/main/java/com/tdd/study/domain/BookRepository.java
+++ src/main/java/com/tdd/study/domain/BookRepository.java
@@ -1,4 +1,7 @@
 package com.tdd.study.domain;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
 }
--- src/test/java/com/tdd/study/domain/BookRepositoryTest.java
+++ src/test/java/com/tdd/study/domain/BookRepositoryTest.java
@@ -1,4 +1,19 @@
 package com.tdd.study.domain;
 
-public interface BookRepositoryTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest // DB 와 관련된 컴포넌트만 메모리에 로딩
+public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    @DisplayName("책 등록 테스트")
+    public void insertBookTest(){
+        System.out.println("책 등록 테스트 실행");
+    }
 }
